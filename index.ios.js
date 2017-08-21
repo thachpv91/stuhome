0d37fd78a4425eef6ff4e7475af35c416d479107
just4fun <houritsunohikari@gmail.com>
2017-08-21 21:19:16 +0800

    remove all stuff

--- index.ios.js
+++
@@ -1,7 +0,0 @@
-// make `Symbol` available on iOS 8
-import 'es6-symbol/implement';
-
-import { AppRegistry } from 'react-native';
-import App from './src/containers/App';
-
-AppRegistry.registerComponent('stuhome', () => App);
